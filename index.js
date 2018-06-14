fd56a73b16287af05c7b700a6c0cb971bef0701b
luowei <luowei14@meituan.com>
2018-06-14 14:49:01 +0800

    bugfix、修改合并规则

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var pipe = require('./util').pipe
 
 // 1. 获取首字母拼音
 const getFirstLetter = (arr) => arr.map((item) => ({
-    [/^[a-zA-Z|\u4e00-\u9fa5]/.test(item) ? pinyin(item,{ style: pinyin.STYLE_FIRST_LETTER })[0][0].toLowerCase() : '其他']: [item]
+    [/^[a-zA-Z|\u4e00-\u9fa5]/.test(item) ? pinyin(item,{ style: pinyin.STYLE_FIRST_LETTER })[0][0].toLowerCase().charAt(0) : '其他']: [item]
 }))
 // 2. 合并重复首字母
 const letterZip = (arr) => arr.reduce((acc, it) => {
@@ -11,10 +11,7 @@ const letterZip = (arr) => arr.reduce((acc, it) => {
         acc[Object.keys(it)[0]] = acc[Object.keys(it)[0]].concat(Object.values(it)[0])
         return acc
     } else {
-        return {
-            ...acc,
-            ...it
-        }
+        return Object.assign(acc, it)
     }
 })
 // 3. 字母排序
