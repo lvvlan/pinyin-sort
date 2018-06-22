344916e84a2dce7281400b3041bbf5c4deef6fa6
luowei <luowei14@meituan.com>
2018-06-22 14:43:40 +0800

    HotFix-增加容错策略

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var pinyin = require('pinyin')
 var pipe = require('./util').pipe
 
 // 1. 获取首字母拼音
-const getFirstLetter = (arr) => arr.map((item) => ({
+const getFirstLetter = (arr) => arr.length ? arr.map((item) => ({
     [/^[a-zA-Z|\u4e00-\u9fa5]/.test(item) ? pinyin(item,{ style: pinyin.STYLE_FIRST_LETTER })[0][0].toLowerCase().charAt(0) : '其他']: [item]
-}))
+})) : []
 // 2. 合并重复首字母
-const letterZip = (arr) => arr.reduce((acc, it) => {
+const letterZip = (arr) => arr.length ? arr.reduce((acc, it) => {
     if (acc[Object.keys(it)[0]]) {
         acc[Object.keys(it)[0]] = acc[Object.keys(it)[0]].concat(Object.values(it)[0])
         return acc
     } else {
         return Object.assign(acc, it)
     }
-})
+}) : {}
 // 3. 字母排序
-const letterSort = (json) => Object.keys(json).sort().map((item) => ({
+const letterSort = (json) => Object.keys(json).length ? Object.keys(json).sort().map((item) => ({
     pinyin: item,
     value: json[item]
-}))
+})) : {}
 
 module.exports = pipe(getFirstLetter, letterZip, letterSort)
